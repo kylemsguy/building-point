9d4853910b3ffe3e98badb0882744789faa3299d
Kyle Zhou <kylezhou2002@hotmail.com>
2014-11-15 14:05:00 -0500

    Finished implementing compass. Currently testing.

--- app/src/main/java/com/kylemsguy/buildingpoint/MainActivity.java
+++ app/src/main/java/com/kylemsguy/buildingpoint/MainActivity.java
@@ -1,19 +1,55 @@
 package com.kylemsguy.buildingpoint;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private float currentDegree = 0f;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    TextView tvHeading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvHeading = (TextView) findViewById(R.id.tvHeading);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+        // Not in use.
+    }
+
+    public void onSensorChanged(SensorEvent event){
+        float degree = Math.round(event.values[0]);
+        tvHeading.setText("Heading: " + Float.toString(degree) + "\u00b0");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
