c1d9a63573e861dc93a73e78f0b29d04172882fb
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2014-11-16 06:05:35 -0500

    Experimental Nod support; angle acquired from the device is still wrong

--- app/src/main/java/com/kylemsguy/buildingpoint/MainActivity.java
+++ app/src/main/java/com/kylemsguy/buildingpoint/MainActivity.java
@@ -1,6 +1,10 @@
 package com.kylemsguy.buildingpoint;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.hardware.GeomagneticField;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -10,15 +14,19 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 import net.zhuoweizhang.bingvenueaccess.model.*;
+
+import net.openspatial.*;
 
 
 public class MainActivity extends Activity implements SensorEventListener, View.OnClickListener, LocationListener {
@@ -38,6 +46,117 @@ public class MainActivity extends Activity implements SensorEventListener, View.
     private Button btnPoint;
     private Location currentLocation;
 
+	/* Nod */
+	public static final String TAG = "NodCompass";
+
+	private boolean nodEnabled = false;
+	private EulerAngle nodEulerAngle;
+	private double nodCalibrateSubtractAngle;
+	private double nodCalibrateAddAngle;
+	private long nodLastDownButton0;
+	private long nodLastDownButton1;
+
+	OpenSpatialService mOpenSpatialService;
+    private ServiceConnection mOpenSpatialServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mOpenSpatialService = ((OpenSpatialService.OpenSpatialServiceBinder)service).getService();
+
+            mOpenSpatialService.initialize(TAG, new OpenSpatialService.OpenSpatialServiceCallback() {
+                @Override
+                public void deviceConnected(BluetoothDevice device) {
+                    try {
+                        mOpenSpatialService.registerForPose6DEvents(device, new OpenSpatialEvent.EventListener() {
+                            @Override
+                            public void onEventReceived(OpenSpatialEvent event) {
+								nodEulerAngle = ((Pose6DEvent) event).getEulerAngle();
+                            }
+                        });
+
+                        mOpenSpatialService.registerForButtonEvents(device, new OpenSpatialEvent.EventListener() {
+                            @Override
+                            public void onEventReceived(OpenSpatialEvent event) {
+                                ButtonEvent bEvent = (ButtonEvent)event;
+								System.out.println("Button event: " + bEvent.buttonEventType);
+
+                                if (bEvent.buttonEventType == ButtonEvent.ButtonEventType.TOUCH0_DOWN &&
+									System.currentTimeMillis() - nodLastDownButton0 > 1000) {
+                                    doQueryPoint();
+									nodLastDownButton0 = System.currentTimeMillis();
+                                } else if (bEvent.buttonEventType == ButtonEvent.ButtonEventType.TOUCH1_DOWN &&
+									System.currentTimeMillis() - nodLastDownButton1 > 1000) {
+                                    System.out.println("Recenter!");
+									Toast.makeText(MainActivity.this, "Recentered!", Toast.LENGTH_SHORT
+).show();
+									if (nodEulerAngle != null) {
+										nodCalibrateSubtractAngle = nodEulerAngle.yaw;
+										nodCalibrateAddAngle = azimuth;
+									}
+									nodLastDownButton1 = System.currentTimeMillis();
+                                }
+                            }
+                        });
+						mOpenSpatialService.registerForGestureEvents(device, new OpenSpatialEvent.EventListener() {
+							@Override
+							public void onEventReceived(OpenSpatialEvent revent) {
+								GestureEvent event = (GestureEvent) revent;
+								System.out.println("Gesture! " + event.gestureEventType);
+								int delta = 0;
+								if (event.gestureEventType == GestureEvent.GestureEventType.SCROLL_DOWN) {
+									delta = 1;
+								} else if (event.gestureEventType == GestureEvent.GestureEventType.SCROLL_UP) {
+									delta = -1;
+								}
+								if (delta != 0) {
+									LocDetailFragment theFragment = ((LocDetailFragment) getFragmentManager().findFragmentByTag("LocDetailFragment"));
+									int newIndex = theFragment.getViewPager().getCurrentItem() + delta;
+									if (newIndex >= 0 && newIndex < theFragment.getViewPager().getAdapter().getCount()) {
+										theFragment.getViewPager().setCurrentItem(newIndex, true);
+									}
+								}
+							}
+						});
+
+                    } catch (OpenSpatialException e) {
+                        System.err.println("Error registering for Pose6DEvent " + e);
+						e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void buttonEventRegistrationResult(BluetoothDevice device, int i) {
+                }
+
+                @Override
+                public void pointerEventRegistrationResult(BluetoothDevice device, int i) {
+                }
+
+                @Override
+                public void pose6DEventRegistrationResult(BluetoothDevice device, int i) {
+
+                }
+
+                @Override
+                public void gestureEventRegistrationResult(BluetoothDevice device, int i) {
+                }
+                @Override
+                public void motion6DEventRegistrationResult(BluetoothDevice device, int i) {
+                }
+				@Override
+				public void deviceDisconnected(BluetoothDevice device) {
+				}
+
+            });
+
+            mOpenSpatialService.getConnectedDevices();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mOpenSpatialService = null;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +170,7 @@ public class MainActivity extends Activity implements SensorEventListener, View.
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         //btnPoint = (Button) findViewById(R.id.btnPoint);
         //btnPoint.setOnClickListener(this);
+		bindService(new Intent(this, OpenSpatialService.class), mOpenSpatialServiceConnection, BIND_AUTO_CREATE);
     }
 
     @Override
@@ -111,7 +231,10 @@ public class MainActivity extends Activity implements SensorEventListener, View.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
-        }
+        } else if (id == R.id.action_toggle_nod) {
+			nodEnabled = !nodEnabled;
+			setTitle(nodEnabled? "BuildingPoint: Nod Enabled!": "BuildingPoint");
+		}
         return super.onOptionsItemSelected(item);
     }
 
@@ -122,7 +245,12 @@ public class MainActivity extends Activity implements SensorEventListener, View.
     }
 
     public void doQueryPoint() {
-        new ProcessPointTask(this, currentLocation, azimuth).execute();
+		float yaw = nodEnabled && nodEulerAngle != null? (float) ((Math.PI * 2) - (nodEulerAngle.yaw - nodCalibrateSubtractAngle) + nodCalibrateAddAngle):
+			azimuth;
+		if (nodEnabled) {
+			setTitle("nod: " + (((nodEulerAngle.yaw - nodCalibrateSubtractAngle) + nodCalibrateAddAngle) % Math.PI )+ " reg: " + azimuth);
+		}
+        new ProcessPointTask(this, currentLocation, yaw).execute();
     }
 
     public void onLocationChanged(Location location) {
